cf2253928d23601d8c6fe8a3e8451fe6bcaddf1f
Home <wjdals7863@naver.com>
2023-11-14 20:32:49 +0900

    V0

--- OnlineCheck.py
+++ OnlineCheck.py
@@ -1,7 +1,7 @@
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-from PyQt6 import uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
 
 import socket, shutil, os, sys, time
 
