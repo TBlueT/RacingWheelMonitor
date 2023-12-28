ac2b63ecf0a9710d5bd122b299f266ae319a17bc
Home <wjdals7863@naver.com>
2023-12-28 15:11:18 +0900

    V2 - Applying mvvm model...

--- ViewModel/DisplayManagementViewModel.py
+++ ViewModel/DisplayManagementViewModel.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5 import QtCore, QtGui, QtTest
 
 from ViewModel.ImageProcessing import ImageProcessing
@@ -16,10 +18,14 @@ class DisplayManagementViewModel(QtCore.QThread):
 
     # def RPMBar_SetMaxRPM(self, maxRpm:int):
     #     self.ImageP.
-
+        self.DisplayUpdateTimeSet = 0.05
+        self.DisplayUpdateTime = time.time()
 
     def run(self):
         while True:
-            self.Set_Pixmap.emit("RPMBar", self.ImageP.RPMBar_GetImg())
-            self.Set_Pixmap.emit("ERS_Store", self.ImageP.ERS_Store_GetImg())
-            self.Set_Pixmap.emit("ERS_Deploted", self.ImageP.ERS_Deploted_GetImg())
+            temp_time = time.time()
+            if temp_time - self.DisplayUpdateTime > self.DisplayUpdateTimeSet:
+                self.Set_Pixmap.emit("RPMBar", self.ImageP.RPMBar_GetImg())
+                self.Set_Pixmap.emit("ERS_Store", self.ImageP.ERS_Store_GetImg())
+                self.Set_Pixmap.emit("ERS_Deploted", self.ImageP.ERS_Deploted_GetImg())
+                self.DisplayUpdateTime = temp_time
