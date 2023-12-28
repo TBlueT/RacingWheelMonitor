1bf0e9d39d8705cdc1adae928425b7b2abc921ee
Home <wjdals7863@naver.com>
2023-12-28 12:51:10 +0900

    V2 - Applying mvvm model...

--- ViewModel/ImageProcessing.py
+++ ViewModel/ImageProcessing.py
@@ -6,18 +6,27 @@ from PyQt5.QtGui import QImage, QPixmap
 class BarImg:
     def __init__(self, size):
         self.size = size
-        self.__img = np.full((self.size[0], self.size[1], 3), (100, 100, 0), dtype=np.uint8)
+        self.__img = np.full((self.size[1], self.size[0], 3), (100, 100, 0), dtype=np.uint8)
 
     @property
     def img(self):
-        return QPixmap.fromImage(QImage(self.__img, self.size[0], self.size[1], self.size[0]*3, QImage.Format_RGB888))
+        h, w, c = self.__img.shape
+        return QPixmap.fromImage(QImage(self.__img,  w, h, w*c, QImage.Format_RGB888))
 
     def Graduation_init(self):
         temp_xPoint = int(self.size[0]/10)
-        temp_yPoint = self.size[1]
-        start_point = (temp_xPoint, 0)
-        end_point = (temp_xPoint, temp_yPoint)
-        self.__img = cv2.line(self.__img, start_point, end_point, (0,255,0), 5)
+        temp_yPoint = int(self.size[1]/6)
+
+        for i in range(1, 10):
+            start_point_1 = (temp_xPoint*i, 0)
+            end_point_1 = (temp_xPoint*i, temp_yPoint)
+            start_point_2 = (temp_xPoint*i, temp_yPoint * 5)
+            end_point_2 = (temp_xPoint*i, self.size[1])
+            self.__img = cv2.line(self.__img, start_point_1, end_point_1, (74, 74, 74), 2)
+            cv2.putText(self.__img, F"{i}", (temp_xPoint*i-5, int(temp_yPoint*4.23)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (74,74,74), 2, cv2.LINE_AA)
+            self.__img = cv2.line(self.__img, start_point_2, end_point_2, (74, 74, 74), 2)
+
+
 
 class ImageProcessing:
     def __init__(self, parent=None):
